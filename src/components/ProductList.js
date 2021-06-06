faa3ced5f6e36c1a784e60c8ebb2ad458a80d255
ziedjalajel <zied.jalajel1993@gmail.com>
2021-06-06 13:15:56 +0300

    done

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,16 +5,22 @@ import ProductItem from "./ProductItem";
 import SearchBar from "./SearchBar";
 // Data
 import products from "../products";
+import {useState} from "react";
+
 
 const ProductList = () => {
-  const productList = products.map((product) => (
+  const [Query, setQuery] = useState("");
+  const productList = products
+  .filter(product=>product.name.toLowerCase().includes(Query.toLowerCase()))
+  .map((product) => (
     <ProductItem product={product} key={product.id} />
   ));
 
   return (
     <>
-      <SearchBar />
+      <SearchBar setQuery={setQuery}/>
       <ListWrapper>{productList}</ListWrapper>
+      
     </>
   );
 };
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,11 @@
-import React from "react";
+
 // Styling
 import { SearchBarStyled } from "../styles";
 
 const SearchBar = (props) => {
   return (
     <SearchBarStyled
-      onChange={(event) => console.log(event.target.value)}
+      onChange = {(event) => props.setQuery(event.target.value)}
       placeholder="Search for a cookie name"
     />
   );
